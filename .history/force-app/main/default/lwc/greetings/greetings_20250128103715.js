e6a126e9480bb404fbfac343ffa5cd00626e23de
deepwadar19 <deepakwadar192@gmail.com>
2025-03-08 18:32:21 +0530

    March Changes

---
+++ .history/force-app/main/default/lwc/greetings/greetings_20250128103715.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class Greetings extends LightningElement {
+    /*greetings = "Hello";
+
+    handleClick(){
+        this.greetings = "Goodbye";
+    }*/
+   name = {first:"John", last:"Doe"};
+   
+}
