8a678536946b07225e7489406695f4fcb00a71a2
deepwadar19 <deepakwadar192@gmail.com>
2025-05-12 14:17:52 +0530

    renderedCallback demo

---
+++ .history/force-app/main/default/lwc/lifeCycleHooksDemo/lifeCycleHooksDemo_20250512104425.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+import 
+
+export default class LifeCycleHooksDemo extends LightningElement {
+
+    constructor(){
+        super();
+        alert('Constructor is Called..');
+    }
+}
--- .history/force-app/main/default/lwc/lifeCycleHooksDemo/lifeCycleHooksDemo_20250512110755.js
+++ .history/force-app/main/default/lwc/lifeCycleHooksDemo/lifeCycleHooksDemo_20250512110755.js
@@ -2,9 +2,6 @@ import { LightningElement, wire } from 'lwc';
 import getAccount from '@salesforce/apex/AccountDetails.getAccountData';
 export default class LifeCycleHooksDemo extends LightningElement {
 
-    helloWorld = 'Sample Value';
-    boolVar = false;
-
     constructor(){
         super();
         alert('Constructor is Called..');
@@ -25,21 +22,6 @@ export default class LifeCycleHooksDemo extends LightningElement {
         //tree structure is not formed
     }
 
-    renderedCallback(){
-        alert('renderedCallback is Called..');
-    }
     
-    handleClick(){
-
-        if(this.helloWorld == 'Sample Value'){
-            this.boolVar = true;
-            this.helloWorld = 'Hello World';
-        }else{
-            this.boolVar = false;
-            this.helloWorld = 'Sample Value';
-        }
-
-        //alert(this.template.querySelector('div')); 
-        // able to access div from template html -tree structure is formed
-    }
+    
 }
