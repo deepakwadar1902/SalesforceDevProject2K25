ff7e586c3a2355730307996e763be39cbdd09d83
deepwadar19 <deepakwadar192@gmail.com>
2025-05-12 15:06:55 +0530

    wire and imperative call

---
+++ .history/force-app/main/default/lwc/wireAndImperative/wireAndImperative_20250512150455.js
@@ -0,0 +1,38 @@
+import { LightningElement, wire } from 'lwc';
+import callingWire from '@salesforce/apex/wireAndImperativeController.getAccountDetails';
+export default class WireAndImperative extends LightningElement {
+
+    /*
+        If you want fresh data on every render, use the wire adapter
+        If you don't want fresh data on every render, use the connectedCallback or imperative adapter
+    */
+
+        myName = 'New';
+        showData;
+
+        @wire(callingWire, {name : '$myName'})
+        wireData({error, data}){
+            alert('Outside If wire');
+            if(data){
+                alert('Inside If wire');
+                this.showData = data.Name;
+            }else if(error){
+                console.log('Error: ' +error );
+            }
+        }
+
+        handleChange(event){
+            this.myName = event.target.value;
+        }
+
+        connectedCallback(){
+            alert('Outside method connectedCallback..');
+            callingWire({name : this.myName})
+            .then((result)=>{
+                alert('Inside method connectedCallback..');
+                this.showData = result.Name;
+            }).catch((error)=>{
+                console.log('Error: ' +error);
+            });
+        }
+}
